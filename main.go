f20b7dde553be4e240a82aa999b2a34653e079e9
BigdawgRav <raveenseshan@live.com>
2021-10-19 15:59:13 +1100

    multiple request

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/apex/gateway"
 	"github.com/gin-gonic/gin"
@@ -20,132 +21,166 @@ func inLambda() bool {
 	return false
 }
 
+//CoinGeckoTicker is a struct that provides a structure to repackage infomation returned from the CoinGecko Api
 type CoinGeckoTicker struct {
 	Base         string  `json:"base"`
 	Target       string  `json:"target"`
 	Last         float64 `json:"last"`
 	Volume       float64 `json:"volume"`
-	CoinId       string  `json:"coin_id"`
-	TargetCoinId string  `json:"target_coin_id"`
+	CoinID       string  `json:"coin_id"`
+	TargetCoinID string  `json:"target_coin_id"`
 }
 
+//CoinGeckoReturnObject is a struct that is used implement the return object from CoinGecko Api
 type CoinGeckoReturnObject struct {
 	Name   string            `json:"name"`
 	Ticker []CoinGeckoTicker `json:"tickers"`
 }
 
-func UpdateLast(updateItem CoinGeckoTicker, currencyValue float64) CoinGeckoTicker {
+// UpdateLast update the currenacy value of the object
+func UpdateLast(updateItem *CoinGeckoTicker, currencyValue float64) {
 	updateItem.Last = currencyValue
-	return updateItem
 }
 
-func UpdateTarget(updateItem CoinGeckoTicker, currencyName string) CoinGeckoTicker {
+//UpdateTarget upates the target value of a coingecko object
+func UpdateTarget(updateItem *CoinGeckoTicker, currencyName string) {
 	updateItem.Target = currencyName
-	return updateItem
+
 }
 
 //check if coingecko currency has required object , if so return the object if not return in USD
-func analyseCoinGeckoReturn(coin_gecko_return_object CoinGeckoReturnObject, fiat string) (CoinGeckoTicker, string) {
+func analyseCoinGeckoReturn(coinGeckoReturnObject CoinGeckoReturnObject, fiat string) (CoinGeckoTicker, string) {
 
-	var converted_cryto_object CoinGeckoTicker
-	var converted_crypto_object_usdt CoinGeckoTicker
-	return_fiat_type := "USDT"
-	ticker_array := coin_gecko_return_object.Ticker
+	var convertedCrytoObject CoinGeckoTicker
+	var convertedCryptoObjectUsdt CoinGeckoTicker
+	returnFiatType := "USDT"
+	tickerArray := coinGeckoReturnObject.Ticker
 
-	for _, s := range ticker_array {
+	for _, s := range tickerArray {
 		if s.Target == fiat {
-			converted_cryto_object = s
-			return converted_cryto_object, s.Target
+			convertedCrytoObject = s
+			return convertedCrytoObject, s.Target
 		}
-		if s.Target == "USDT" {
-			converted_crypto_object_usdt = s
+		if s.Target == "USD" {
+			convertedCryptoObjectUsdt = s
+		} else if s.Target == "USDT" {
+			convertedCryptoObjectUsdt = s
+		} else if s.Target == "USDC" {
+			convertedCryptoObjectUsdt = s
 		}
 	}
-	if converted_cryto_object == (CoinGeckoTicker{}) {
-		converted_cryto_object = converted_crypto_object_usdt
+	if convertedCrytoObject == (CoinGeckoTicker{}) {
+		convertedCrytoObject = convertedCryptoObjectUsdt
 	}
-
-	return converted_cryto_object, return_fiat_type
+	return convertedCrytoObject, returnFiatType
 }
 
 // convert fiat currency
-func convertToRequiredFiatCurrency(usdt_value float64, required_fiat_type string) float64 {
+func convertToRequiredFiatCurrency(usdtValue float64, requiredFiatType string, c *gin.Context) float64 {
 
-	s := fmt.Sprintf("%f", usdt_value)
-	currency_exchange_api_call_link := "https://currency-exchange.p.rapidapi.com/exchange?to=" + required_fiat_type + "&from=USD&Q=1" + s
+	// s := fmt.Sprintf("%f", usdt_value)
+	currencyExchangeAPICALLLink := "https://currency-exchange.p.rapidapi.com/exchange?to=" + requiredFiatType + "&from=USD=1"
 
 	// make api call
-	req, _ := http.NewRequest("GET", currency_exchange_api_call_link, nil)
+	req, _ := http.NewRequest("GET", currencyExchangeAPICALLLink, nil)
 
 	req.Header.Add("x-rapidapi-host", "currency-exchange.p.rapidapi.com")
 	req.Header.Add("x-rapidapi-key", "e152fe0ac7msh82be55889f4e392p160cd0jsn2ee8bf76ee34")
-	res, _ := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"currency change": "currency change"})
+	}
 
 	defer res.Body.Close()
+
 	rate, _ := ioutil.ReadAll(res.Body)
 
-	rate_in_string := string(rate)
-	rate_in_number, _ := strconv.ParseFloat(rate_in_string, 64)
+	rateInString := string(rate)
+	rateInNumber, _ := strconv.ParseFloat(rateInString, 64)
 
-	converted_value := rate_in_number * usdt_value
+	convertedValue := rateInNumber * usdtValue
 
-	return converted_value
+	return convertedValue
 }
 
-func getCurrencyCurrentPrice(c *gin.Context) {
+// Gets a single coins price in desired currency
+func getCoinGeckoUnitPrice(id string, exchange string, fiat string, c *gin.Context) (coinInfo *CoinGeckoTicker, err error) {
 
-	id := c.Param("id")
-	fiat := c.Query("fiat")
-	exchange := c.Query("exchange")
+	coinGeckoCallURL := "https://api.coingecko.com/api/v3/exchanges/" + exchange + "/tickers?coin_ids=" + id
 
-	if len(id) == 0 || len(fiat) == 0 || len(exchange) == 0 {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"paramters": "parameters missing"})
-	}
-	// Url coin gecko url
-	coin_gecko_call_url := "https://api.coingecko.com/api/v3/exchanges/" + exchange + "/tickers?coin_ids=" + id
-
-	//HTTP CALL
-	resp, err := http.Get(coin_gecko_call_url)
+	response, err := http.Get(coinGeckoCallURL)
 	if err != nil {
+		return nil, err
 
-		c.IndentedJSON(http.StatusNotFound, gin.H{"coingecko": "error"})
 	}
 
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
-	//Decode JSON
-	coin_gecko_return_object := &CoinGeckoReturnObject{}
-	dec := json.NewDecoder(resp.Body)
+	coinGeckoReturnObject := &CoinGeckoReturnObject{}
+	dec := json.NewDecoder(response.Body)
 
-	if err := dec.Decode(coin_gecko_return_object); err != nil {
+	if err := dec.Decode(coinGeckoReturnObject); err != nil {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"coingecko": "Decode error"})
-	} else {
+		return nil, err
+	}
 
-		return_ticker_object := *coin_gecko_return_object
-		crypto_object_to_return, extracted_fiat := analyseCoinGeckoReturn(return_ticker_object, fiat)
+	returnTickerObject := *coinGeckoReturnObject
+	cryptoObjectToReturn, extractedFiat := analyseCoinGeckoReturn(returnTickerObject, fiat)
 
-		if extracted_fiat != fiat {
-			if extracted_fiat == "USDT" {
-				currency_usd_value := crypto_object_to_return.Last
-				required_fiat_type := fiat
-				converted_to_required_value := convertToRequiredFiatCurrency(currency_usd_value, required_fiat_type)
+	if extractedFiat != fiat {
+		if extractedFiat == "USDT" || extractedFiat == "USD" || extractedFiat == "USDC" {
+			currencyUsdValue := cryptoObjectToReturn.Last
+			requiredFiatType := fiat
 
-				// update object
-				itemWithLastUpdated := UpdateLast(crypto_object_to_return, converted_to_required_value)
-				itemWithTargetUpdated := UpdateTarget(itemWithLastUpdated, fiat)
-				c.IndentedJSON(http.StatusOK, itemWithTargetUpdated)
+			convertedToRequiredValue := convertToRequiredFiatCurrency(currencyUsdValue, requiredFiatType, c)
 
-			}
-		} else {
-			c.IndentedJSON(http.StatusOK, crypto_object_to_return)
+			// update object
+			UpdateLast(&cryptoObjectToReturn, convertedToRequiredValue)
+			UpdateTarget(&cryptoObjectToReturn, fiat)
 
 		}
 	}
+
+	return &cryptoObjectToReturn, nil
+}
+
+// Url routed view function
+func getCurrencyCurrentPrice(c *gin.Context) {
+
+	fiat := c.Query("fiat")
+
+	exchangeList := strings.Split(c.Query("exchangelist"), ",")
+	cryptoList := strings.Split(c.Query("cryptolist"), ",")
+
+	if len(exchangeList) == 0 || len(fiat) == 0 || len(cryptoList) == 0 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"paramters": "parameters missing"})
+	}
+
+	// send multiple request
+
+	coinMap := make(map[int]*CoinGeckoTicker, len(cryptoList))
+
+	for i := 0; i < len(cryptoList); i++ {
+
+		id := cryptoList[i]
+		exchange := exchangeList[i]
+
+		coin, err := getCoinGeckoUnitPrice(id, exchange, fiat, c)
+		if err != nil {
+			continue
+		}
+
+		coinMap[i] = coin
+
+	}
+	c.IndentedJSON(http.StatusOK, coinMap)
+
 }
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
-	router.GET("/currency/:id", getCurrencyCurrentPrice)
+	router.GET("/currency", getCurrencyCurrentPrice)
 	return router
 }
 
